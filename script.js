65468761738345ae7f7a344f096fe7cba4c7f03c
athu <athulyas2024@gmail.com>
2024-12-19 15:49:59 +0530

    commit server

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-const addButton = document.getElementById('addButton');
-const taskInput = document.getElementById('taskInput');
-const taskList = document.getElementById('taskList');
-
-addButton.addEventListener("click", function(){
-    const taskText = taskInput.value.trim();
-    if(taskInput!==""){
-        const listItem = document.createElement("li");
-        const taskContent = document.createElement("span");
-        taskContent.innerText=taskText;
-
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent="Delete";
-        deleteBtn.addEventListener("click", function(){
-            taskList.removeChild(listItem);
-        });
-
+async function handleSubmit() {
+    const todo = document.getElementById('todo').value;
+    try {
+        const response =await fetch('http://localhost:3000/', {
+            method:"POST",
+            header:{"Content-type": 'application/json'},
+            body:JSON.stringify(todo)
+        })
         
-        listItem.appendChild(taskContent);
-        listItem.appendChild(deleteBtn);
-        taskList.appendChild(listItem);
-    }else {
-        alert("Please Enter a task")
+    } catch (error) {
+        console.log(error)
     }
-    
-});
+}
